35b0e56859db1ea8f7eda2e10ac6e284b3279851
Hoel Bagard <hoel.bagard@gmail.com>
2021-08-18 14:04:55 +0900

    Formatted readme, worked a bit on the flattening script (not happy with it though).

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Utility script for handling COCO-style data
 Requires python version >= 3.9
 
 ### Dependencies
-opencv-python
-matplotlib
+opencv-python\
+matplotlib\
 pycocotools
 
 
@@ -20,30 +20,54 @@ TODO
 
 #### Merge
 If necessary merge several coco json with:
-```python utils/merge_coco.py <path_to_first_json> <path_to_second_json>```
-```python utils/merge_coco.py ../data/second_dataset/vott-json-export_brown/coco_annotations.json ../data/second_dataset/vott-json-export_blue/coco_annotations.json ../data/second_dataset/first_dataset/coco_annotations.json```
+```
+python utils/merge_coco.py <path_to_first_json> <path_to_second_json>
+```
+```
+python utils/merge_coco.py ../data/second_dataset/vott-json-export_brown/coco_annotations.json ../data/second_dataset/vott-json-export_blue/coco_annotations.json ../data/second_dataset/first_dataset/coco_annotations.json
+```
 
 #### Resize
 Resize the dataset (if needed):
-```python utils/resize_coco.py <path_to_image_dir> <path_to_json_annotations> <output_path> <size1> <size2>```
-```python utils/resize_coco.py ../data/original_dataset/train/images/ ../data/original_dataset/train/annotations.json ../data/resized_dataset/train 550 550```
+```
+python utils/resize_coco.py <path_to_image_dir> <path_to_json_annotations> <output_path> <size1> <size2>
+```
+```
+python utils/resize_coco.py ../data/original_dataset/train/images/ ../data/original_dataset/train/annotations.json ../data/resized_dataset/train 550 550
+```
 
 #### Rename
 Change all the ids from strings to ints:
-```python utils/coco_ids_to_int.py <path_to_annotation_file>```
-```python utils/coco_ids_to_int.py ../data/train/annotations.json```
+```
+python utils/coco_ids_to_int.py <path_to_annotation_file>
+```
+```
+python utils/coco_ids_to_int.py ../data/train/annotations.json
+```
 
 #### Grayscale (remove that part ?)
 Convert images to grayscale if desired:
-```python utils/imgs_to_grayscale.py <path_to_image_folder>```
-```python utils/imgs_to_grayscale.py ../data/validation/images/```
+```
+python utils/imgs_to_grayscale.py <path_to_image_folder>
+```
+```
+python utils/imgs_to_grayscale.py ../data/validation/images/
+```
 
 #### Split
 Split the dataset into train and validation datasets:
-```python utils/split_train_val.py <path to image folder> <path to annotation file> <output path>```
-```python utils/split_train_val.py ../data/original_dataset ../data/original_dataset/coco_annotations.json ../data/split_dataset/```
+```
+python utils/split_train_val.py <path to image folder> <path to annotation file> <output path>
+```
+```
+python utils/split_train_val.py ../data/original_dataset ../data/original_dataset/coco_annotations.json ../data/split_dataset/
+```
 
 #### Visualize the data
 Finally, check that everything works as expected by using:
-```python utils/visualize_coco_data.py <path to image folder> <path to annotation file>```
-```python utils/visualize_coco_data.py ../data/train/images/ ../data/train/annotations.json```
+```
+python utils/visualize_coco_data.py <path to image folder> <path to annotation file>
+```
+```
+python utils/visualize_coco_data.py ../data/train/images/ ../data/train/annotations.json
+```
--- coco_utils/flatten_data_structure.py
+++ coco_utils/flatten_data_structure.py
@@ -1,5 +1,7 @@
 from argparse import ArgumentParser
+import xml.etree.ElementTree as ET
 from pathlib import Path
+import random
 import shutil
 
 
@@ -17,36 +19,58 @@ def main():
     move: bool = args.move
     img_output_path: Path = args.output_path if args.output_path else args.data_path.parent / "flattened_dataset"
 
+    moving_fn = shutil.move if move else shutil.copy  # Shortcut to avoid ifs later
+
     # Output paths are a bit diffenrent if also moving the labels
     if labels_too:
         img_output_path = img_output_path / "imgs"
         labels_output_path = img_output_path / "labels"
         labels_output_path.mkdir(parents=True, exist_ok=True)
-
     img_output_path.mkdir(parents=True, exist_ok=True)
 
     exts = (".png", ".jpg", ".bmp")
     img_paths = list([path for path in data_path.rglob('*') if path.suffix in exts])
-
     nb_imgs = len(img_paths)
     for i, img_path in enumerate(img_paths, start=1):
         msg = f"Processing image: {img_path.name} ({i}/{nb_imgs})"
         print(msg + ' ' * (shutil.get_terminal_size(fallback=(156, 38)).columns - len(msg)), end='\r', flush=True)
 
         file_output_path = img_output_path / img_path.name
+        xml_modified = False
+        xml_path: Path = None
 
-        # Check if xml exists
-
+        # Check if xml exists (either next to the image or in an adjacent "labels" folder)
+        if img_path.with_suffix(".xml").exists():
+            xml_path = img_path.with_suffix(".xml")
+        elif (img_path.parent.parent / "labels" / (img_path.stem + ".xml")).exists():
+            xml_path = img_path.parent.parent / "labels" / (img_path.stem + ".xml")
 
         if file_output_path.exists():
-            print(f"\nFile {file_output_path} already exists. Skipping.")
+            # There is already an image and xml label file with that name. Rename and the move.
+            if xml_path:
+                xml_modified = True
+                tree = ET.parse(xml_path)
 
-            # Rename file and modify xml accordingly
+                while file_output_path.exists():
+                    file_output_path = file_output_path.with_stem(file_output_path.stem + f"_{random.randint(0, 1000)}")
+                    xml_path = xml_path.with_stem(file_output_path.stem)
 
-        if move:
-            shutil.move(img_path, file_output_path)
-        else:
-            shutil.copy(img_path, file_output_path)
+                root: ET.Element = tree.getroot()
+                root.find("filename").text = file_output_path.name
+
+            # There is already an image with that name.
+            # But no xml (maybe a coco file somewhere else, but then it's already too late)
+            else:
+                print(f"\nFile {file_output_path} already exists. Skipping. "
+                      "You might want to look into that, as the coco file is probably 'wrong'.")
+                continue
+
+        moving_fn(img_path, file_output_path)
+        if xml_path and not xml_modified:
+            moving_fn(xml_path, labels_output_path / xml_path.name)
+        elif xml_modified:
+            with open(labels_output_path / xml_path.name, "wb") as f:
+                tree.write(f)
 
     print("\nFinished!")
 
