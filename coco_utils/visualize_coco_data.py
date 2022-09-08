47ce3f8716f90800faf6ce990274c6edc0baa9b8
Hoel Bagard <hoel.bagard@gmail.com>
2022-09-08 14:50:51 +0900

    Began removing pycocotools

--- coco_utils/visualize_coco_data.py
+++ coco_utils/visualize_coco_data.py
@@ -1,10 +1,11 @@
 import argparse
+import json
 import shutil
 from pathlib import Path
 
 import cv2
 import matplotlib.pyplot as plt
-from pycocotools.coco import COCO
+from coco_types import Annotation, Category, Image
 
 
 def main():
@@ -19,30 +20,34 @@ def main():
     json_path: Path = args.json_path
     show_bbox: bool = args.show_bbox
 
-    coco = COCO(json_path)
+    with open(json_path, 'r', encoding="utf-8") as annotations:
+        coco_dataset = json.load(annotations)
+
+    img_entries: list[Image] = coco_dataset["images"]
+    annotations: list[Annotation] = coco_dataset["annotations"]
+    categories: list[Category] = coco_dataset["categories"]
 
     # Get all images containing given categories, select one at random
-    cat_ids = coco.getCatIds(catNms=["Thread"])
-    img_ids = coco.getImgIds(catIds=cat_ids)
+    nb_imgs = len(img_entries)
+    for i, img_entry in enumerate(img_entries, start=1):
+        msg = f"Showing image: {img_entry['file_name']} ({i+1}/{nb_imgs})"
+        print(msg + ' ' * (shutil.get_terminal_size(fallback=(156, 38)).columns - len(msg)),
+              end='\r' if i != nb_imgs else '\n', flush=True)
 
-    for i in range(len(img_ids)):
-        img_data = coco.loadImgs([img_ids[i]])[0]
-        msg = f"Showing image: {img_data['file_name']} ({i+1}/{len(img_ids)})"
-        print(msg + ' ' * (shutil.get_terminal_size(fallback=(156, 38)).columns - len(msg)), end='\r', flush=True)
+        img = cv2.imread(str(data_path / img_entry["file_name"]))
 
-        ann_ids = coco.getAnnIds(imgIds=[img_data["id"]])
-        anns = coco.loadAnns(ann_ids)
-
-        # Load an image and its corresponding instance annotations then display it
-        img = cv2.imread(str(data_path / img_data["file_name"]))
-
+        img_annotations = [annotation for annotation in annotations
+                           if annotation["image_id"] == img_entry["id"]]
         if show_bbox:
-            # Add the bounding box to the image
-            for annotation in anns:
+            # Add the bounding boxes to the image
+            for annotation in img_annotations:
                 top_x, top_y, width, height = annotation["bbox"]
-                img = cv2.rectangle(img, (int(top_x), int(top_y)), (int(top_x+width), int(top_y+height)),
+                img = cv2.rectangle(img,
+                                    (int(top_x), int(top_y)),
+                                    (int(top_x+width), int(top_y+height)),
                                     (255, 0, 0), 5)
 
+        # Add the segmentation masks
         plt.imshow(img)
         plt.axis('off')
         coco.showAnns(anns)
@@ -63,3 +68,46 @@ if __name__ == "__main__":
 #             pts = np.asarray([[point["x"], point["y"]] for point in points], dtype=np.int32)
 #             pts = pts.reshape((-1, 1, 2))
 #             img = cv2.fillPoly(img, [pts], color)
+
+
+# Old version using pycocotools
+# def main():
+#     parser = argparse.ArgumentParser(description="Tool to visualize coco labels.",
+#                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+#     parser.add_argument("data_path", type=Path, help="Path to the directory with the images.")
+#     parser.add_argument("json_path", type=Path, help="Path to the json file with the coco annotations.")
+#     parser.add_argument("--show_bbox", "-sb", action="store_true", help="Show the bounding boxes.")
+#     args = parser.parse_args()
+
+#     data_path: Path = args.data_path
+#     json_path: Path = args.json_path
+#     show_bbox: bool = args.show_bbox
+
+#     coco = COCO(json_path)
+
+#     # Get all images containing given categories, select one at random
+#     cat_ids = coco.getCatIds(catNms=["Thread"])
+#     img_ids = coco.getImgIds(catIds=cat_ids)
+
+#     for i in range(len(img_ids)):
+#         img_data = coco.loadImgs([img_ids[i]])[0]
+#         msg = f"Showing image: {img_data['file_name']} ({i+1}/{len(img_ids)})"
+#         print(msg + ' ' * (shutil.get_terminal_size(fallback=(156, 38)).columns - len(msg)), end='\r', flush=True)
+
+#         ann_ids = coco.getAnnIds(imgIds=[img_data["id"]])
+#         anns = coco.loadAnns(ann_ids)
+
+#         # Load an image and its corresponding instance annotations then display it
+#         img = cv2.imread(str(data_path / img_data["file_name"]))
+
+#         if show_bbox:
+#             # Add the bounding box to the image
+#             for annotation in anns:
+#                 top_x, top_y, width, height = annotation["bbox"]
+#                 img = cv2.rectangle(img, (int(top_x), int(top_y)), (int(top_x+width), int(top_y+height)),
+#                                     (255, 0, 0), 5)
+
+#         plt.imshow(img)
+#         plt.axis('off')
+#         coco.showAnns(anns)
+#         plt.show()
