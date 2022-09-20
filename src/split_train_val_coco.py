a349d1e804646a1db88d5af15291fa765a104d76
Hoel Bagard <hoel.bagard@gmail.com>
2022-09-20 23:43:11 +0900

    Small refactor.

--- src/split_train_val_coco.py
+++ src/split_train_val_coco.py
@@ -2,39 +2,48 @@ import argparse
 import json
 import shutil
 from pathlib import Path
+from typing import Optional
 
 import numpy as np
+
+from src.types.coco_types import Annotation, Category, Image
 
 
 def main():
     parser = argparse.ArgumentParser(description="Splits COCO annotations file into training and validation sets.",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("data_path", type=Path, help="Path to the directory with the images.")
-    parser.add_argument("annotations", type=Path, help="Path to COCO annotations file.")
-    parser.add_argument("output_path", type=Path, help="Where to store the new Train and Validation datasets")
+    parser.add_argument("annotations_path", type=Path, help="Path to COCO annotations json file.")
+    parser.add_argument("output_path", type=Path, help="Where to store the new Train and Validation datasets.")
     parser.add_argument("--spec_file", "-sf", type=Path, default=None,
                         help="Path to a text file that specifies which images to use for val (one name per line)")
     parser.add_argument("--split", "-s", type=float, default=0.85, help="Train split ratio")
     args = parser.parse_args()
 
+    data_path: Path = args.data_path
+    annotations_path: Path = args.annotations_path
+    output_path: Path = args.output_path
+    spec_file: Optional[Path] = args.spec_file
+    split: float = args.split
+
     # Load the dataset
-    with open(args.annotations, encoding="utf-8") as annotations:
+    with open(annotations_path, encoding="utf-8") as annotations:
         coco_dataset = json.load(annotations)
     images = np.asarray(coco_dataset["images"])
-    annotations = coco_dataset["annotations"]
-    categories = coco_dataset["categories"]
+    annotations: list[Annotation] = coco_dataset["annotations"]
+    categories: list[Category] = coco_dataset["categories"]
 
-    if not args.spec_file:
+    if not spec_file:
         number_of_images = len(images)
         indexes = np.arange(number_of_images)
         np.random.shuffle(indexes)
 
         # Split val / train
-        train_images = list(images[indexes[:int(number_of_images*args.split)]])
-        val_images = list(images[indexes[int(number_of_images*args.split):]])
+        train_images: list[Image] = list(images[indexes[:int(number_of_images*split)]])
+        val_images: list[Image] = list(images[indexes[int(number_of_images*split):]])
     else:
         val_img_names = []
-        with open(args.spec_file, 'r', encoding="utf-8") as spec_file:
+        with open(spec_file, 'r', encoding="utf-8") as spec_file:
             for line in spec_file:
                 val_img_names.append(line.strip())
 
@@ -47,8 +56,8 @@ def main():
                 train_images.append(image)
 
     train_image_ids = [image["id"] for image in train_images]
-    train_annotations = []
-    val_annotations = []
+    train_annotations: list[Annotation] = []
+    val_annotations: list[Annotation] = []
 
     for annotation in annotations:
         if annotation["image_id"] in train_image_ids:
@@ -62,7 +71,7 @@ def main():
         "annotations": train_annotations,
         "categories": categories
     }
-    train_output_path: Path = args.output_path / "train"
+    train_output_path: Path = output_path / "train"
     train_output_path.mkdir(parents=True, exist_ok=True)
     with open(train_output_path / "annotations.json", 'w', encoding="utf-8") as json_file:
         json.dump(train_dataset, json_file, indent=4)
@@ -73,7 +82,7 @@ def main():
         "annotations": val_annotations,
         "categories": categories
     }
-    val_output_path: Path = args.output_path / "validation"
+    val_output_path: Path = output_path / "validation"
     val_output_path.mkdir(parents=True, exist_ok=True)
     with open(val_output_path / "annotations.json", 'w', encoding="utf-8") as json_file:
         json.dump(val_dataset, json_file, indent=4)
@@ -82,12 +91,12 @@ def main():
 
     print("Now moving images. . .", end="\r")
     for image in train_images:
-        in_img_path: Path = args.data_path / image["file_name"]
+        in_img_path: Path = data_path / image["file_name"]
         out_img_path: Path = train_output_path / "images"
         out_img_path.mkdir(exist_ok=True)
         shutil.copy(in_img_path, out_img_path)
     for image in val_images:
-        in_img_path: Path = args.data_path / image["file_name"]
+        in_img_path: Path = data_path / image["file_name"]
         out_img_path: Path = val_output_path / "images"
         out_img_path.mkdir(exist_ok=True)
         shutil.copy(in_img_path, out_img_path)
