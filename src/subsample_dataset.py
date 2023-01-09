a7fb95bfc3f3476fe850ebe5d308286d7ef9c8ce
Hoel Bagard <hoel.bagard@gmail.com>
2023-01-09 19:18:49 +0900

    Allowed specifying the image folder path, and made the selection condition an or.

--- src/subsample_dataset.py
+++ src/subsample_dataset.py
@@ -10,6 +10,8 @@ def main():
     parser = argparse.ArgumentParser(description="Create a smaller dataset by keeping a smaller id range.",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("annotations_path", type=Path, help="Path to the COCO annotations file.")
+    parser.add_argument("--images_folder_path", "-i", type=Path, default=None,
+                        help="Path to the folder with the images. Defaults to annotations_path/../images")
     parser.add_argument("--output_path", "-o", type=Path, default=None,
                         help="Where to store the new dataset, defaults to annotations_path/../smaller_dataset")
     parser.add_argument("--max_id", "-m", type=int, default=None, help="Images with id above max_id will be removed.")
@@ -17,6 +19,7 @@ def main():
     args = parser.parse_args()
 
     annotations_path: Path = args.annotations_path
+    images_path: Path = args.images_folder_path if args.images_folder_path else annotations_path.parent / "images"
     output_path: Path = args.output_path if args.output_path else annotations_path.parent / "smaller_dataset"
     max_id: int | None = args.max_id
     ids_to_keep: list[int] | None = args.keep_ids
@@ -35,11 +38,11 @@ def main():
     new_annotations = [annotation
                        for annotation in annotations
                        if (max_id is not None and annotation["image_id"] < max_id)
-                       and (ids_to_keep is not None and annotation["image_id"] in ids_to_keep)]
+                       or (ids_to_keep is not None and annotation["image_id"] in ids_to_keep)]
     new_images = [image
                   for image in images
                   if (max_id is not None and image["id"] < max_id)
-                  and (ids_to_keep is not None and image["id"] in ids_to_keep)]
+                  or (ids_to_keep is not None and image["id"] in ids_to_keep)]
 
     # Save new newing annotations
     new_dataset = {
@@ -54,7 +57,7 @@ def main():
 
     print("Now copying images. . .", end="\r")
     for image in new_images:
-        in_img_path: Path = annotations_path.parent / "images" / image["file_name"]
+        in_img_path: Path = images_path / image["file_name"]
         out_img_path: Path = output_path / "images"
         out_img_path.mkdir(exist_ok=True)
         shutil.copy(in_img_path, out_img_path)
